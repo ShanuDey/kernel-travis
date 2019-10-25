7c7b3acb9ae3838f657f32168f186b06f875af9c
iamsaalim <40296389+iamsaalim@users.noreply.github.com>
2019-10-25 23:17:16 +0530

    Create gcp.sh

---
+++ gcp.sh
@@ -0,0 +1,27 @@
+! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+! unzip -qq -n ngrok-stable-linux-amd64.zip
+! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen vim neofetch dialog tmux nyancat > /dev/null
+! echo root:uditkarode | chpasswd
+! mkdir -p /var/run/sshd
+! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+! echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+! echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+
+#Run sshd
+get_ipython().system_raw('/usr/sbin/sshd -D &')
+
+#Ask token
+print("Copy authtoken from https://dashboard.ngrok.com/auth")
+authtoken="YOUR_TOKEN_HERE"
+! git config --global user.email "your@email"
+! git config --global user.name "Your Name"
+
+#Create tunnel
+get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
+#Print root password
+print("Root password: uditkarode")
+#Get public address
+# ssh root@0.tcp.ngrok.io -p 10965 <-- port number dictated in colab output
+! curl -s http://localhost:4040/api/tunnels | python3 -c \
+    "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"

43501e48fa6835ee54616df91ad8114341f7cf51
iamsaalim <40296389+iamsaalim@users.noreply.github.com>
2019-10-26 10:08:02 +0530

    Change name

--- gcp.sh
+++ gcp.sh
@@ -1,7 +1,7 @@
 ! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 ! unzip -qq -n ngrok-stable-linux-amd64.zip
 ! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen vim neofetch dialog tmux nyancat > /dev/null
-! echo root:uditkarode | chpasswd
+! echo root:iamsaalim | chpasswd
 ! mkdir -p /var/run/sshd
 ! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 ! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
@@ -20,7 +20,7 @@ authtoken="YOUR_TOKEN_HERE"
 #Create tunnel
 get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
 #Print root password
-print("Root password: uditkarode")
+print("Root password: iamsaalim")
 #Get public address
 # ssh root@0.tcp.ngrok.io -p 10965 <-- port number dictated in colab output
 ! curl -s http://localhost:4040/api/tunnels | python3 -c \
